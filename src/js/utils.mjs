1d0c16deb5c5e1467811749c6ad71cb68637fdb4
gerardalessandrov <gerardvigorodrigues@gmail.com>
2025-11-13 14:15:18 -0500

    index.html

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -15,7 +15,15 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
-
+export function getParam(param){
+  const queryString =window.location.search;
+  // Get the only the string of the query not the URL
+  const urlParams= new URLSearchParams(queryString)
+  // Params that you can analize
+  // Get Values and append values
+  return urlParams.get(param)
+  // Return the value of the param that you stablished like param
+}
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
